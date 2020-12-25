1b40550d255e087a4058da91e93f766b33a38cda
subash774 <subash.poudyal8@gmail.com>
2020-12-25 16:58:49 +0000

    listener and key press done

--- caffeinate/caffeinate.py
+++ caffeinate/caffeinate.py
@@ -1,14 +1,67 @@
-from pynput.keyboard import Key, Controller
+from threading import Thread
+from pynput.keyboard import Key, Controller, Listener
+import argparse
 import time
+from threading import Thread
+from datetime import date, datetime
 
 
-def stay_awake():
-    keyboard = Controller()
-    while True:
-        print("Serving caffeine...press any key to stop")
-        time.sleep(5)
-        keyboard.press(Key.shift)
-        keyboard.release(Key.shift)
+hist = []
+last_online = datetime.now()
 
-if __name__ == "__main__":
-    stay_awake()
+keyboard = Controller()
+
+
+def on_press(key):
+    pass
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    global hist
+    
+    if key == Key.esc:
+        hist.append(True)
+
+        # if esc pressed 3 times, exit
+        if len(hist) == 3:
+        # Stop listener
+            return False
+    else:
+        hist = []
+
+
+def stay_awake(wait):
+    global last_online
+    print(f"Serving caffeine every {wait} seconds")
+    while len(hist) < 3:
+        if (datetime.now() - last_online).seconds > wait:
+            x = last_online - datetime.now()
+            keyboard.press(Key.shift)
+            keyboard.release(Key.shift)
+            last_online = datetime.now()
+        time.sleep(1)
+    return False
+
+
+def run():
+    wait = 90
+    try:
+        parser=argparse.ArgumentParser()
+        parser.add_argument('--time', help='Activate after certain time in seconds')
+        args=parser.parse_args()
+        wait = int(args.time)
+
+    except Exception as e:
+        print(f"No time argument passed, serving caffeine every {wait} secs")
+        pass
+
+    listener = Listener(
+        on_press=on_press,
+        on_release=on_release)
+    awake = Thread(target=stay_awake, args=(wait,))
+
+    listener.start()
+    awake.start()
+    listener.join()
+    awake.join()
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
    zip_safe=False,
    entry_points={
         'console_scripts': [
-            'awake=caffeinate.caffeinate:stay_awake',
+            'awake=caffeinate.caffeinate:run',
         ]
     }
 )
